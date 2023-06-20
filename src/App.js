1a8a5bd36303eb0b219d08edd4d95fb259f88e72
arash sadeghi <arash.sadeghi1997.com>
2023-06-20 20:22:28 -0230

    pyodide working. just fixing double load issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css';
+import { useEffect } from 'react';
+
+// const runScript = async (code) => {
+//   const pyodide = await window.loadPyodide({
+//     indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+//   });
+
+//   return await pyodide.runPythonAsync(code);
+// }
 
 function App() {
+
+  useEffect(()=> {
+    console.log("in useEffect")
+    const pyodide = window.loadPyodide({
+      indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+    });
+  
+  },[])
+  
+
+  // async function main() {
+  //   await runScript(`
+  //       # Your Python code goes here
+  //       print('heee')
+  //   `);
+  // }
+
+
+  const buttonFunction = () =>{
+    console.log('will execute');
+
+    const pyCode = `print('hi from python')`;
+
+    // main();
+    
+    // const pyodide = window.pyodide;
+
+    // pyodide.runPython(pyCode);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-
+      <button onClick={buttonFunction}>Execute Python</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  /* 
+  React.Strict mode is on
+
+    StrictMode renders components twice (on dev but not production) in order to detect any problems with your code and warn you about them (which can be quite useful).
+
+  This answer was pointed out by @johnhendirx and written by @rangfu, see link and give him some love if this was your problem. If you're having issues 
+  because of  this it usually means you're not using useEffect for its intended purpose. There's some great information about this in the beta docs you can read that here
+   */
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
